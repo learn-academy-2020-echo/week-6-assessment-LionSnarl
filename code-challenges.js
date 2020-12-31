487f4e14049c6cb6f58b4b798fd27b7ebfd68cdd
LionSnarl <60535870+LionSnarl@users.noreply.github.com>
2020-12-31 11:40:05 -0800

    Testing commit-Finish question 1

--- code-challenges.js
+++ code-challenges.js
@@ -9,8 +9,52 @@ var people = [
   { name: "arthur dent", occupation: "radio employee" }
 ]
 // Expected output example: "Ford Prefect is a hitchhiker." "Zaphod Beeblebrox is a president of the galaxy." "Arthus Dent is a radio employee."
+// var re = /(\b[a-z](?!\s))/g
+// var s = namemap.replace(re, function(x){return x.toUpperCase()})
+// console.log();
 
+// const persons = (array) => {
+//   let re = /(\b[a-z](?!\s))/g
+//   let namemap = people.map(value => value.name.join(" ").replace(re, function(x){return x.toUpperCase()}))
+//   // let s = namemap.replace(re, function(x){return x.toUpperCase()})
+//   // return s
+//   return namemap
+// }
+// console.log(persons(people));
 
+const persons = (array) => {
+  let namemap = people.map(value => value.name)
+  let re = /(\b[a-z](?!\s))/g
+  // let s = namemap.replace(re, function(x){return x.toUpperCase()})
+  // return s
+  return namemap.join(" ").replace(re, function(x){return x.toUpperCase()})
+}
+console.log(persons(people));
+
+// '${name} is a ${occupation}.'
+
+// V-Brain Storm-V
+// array of key value pairs
+// destructure people
+//var persons = people.filter(value => value )
+//console.log(persons);
+//console.log(people);
+//console.log(people[0]);
+//console.log(people[0].name);
+//$ford prefect
+
+//console.log(people[0].occupation);
+//$hitchhiker
+
+/*const nameCapilizer = (array) => {
+  let names = people.name
+  
+}
+
+*/
+// .map on key pair name and @ Index of 0 .UpperCase
+
+// return through string interpulation key value pairs
 
 
 
@@ -19,6 +63,19 @@ var people = [
 var testingArray1 = [23, "Heyyyy!", 45, -9, 0, "Yo", false]
 // Expected output: [ 2, 0, -0, 0 ]
 var testingArray2 = [5, "Hola", 43, -34, "greetings", true]
+// Expected output: [ 2, 1, -1 ]
+
+// create a local variable that filters for Numbers only, must use Number.isFinite because javascript sees zeros as a falsey statement
+//console log [ 23, 45, -9, 0 ] & [ 5, 43, -34 ]
+// return a .map over the local variable for the remainder of 3 (aka % 3)
+//console log [ 2, 0, -0, 0 ] & [ 2, 1, -1 ]
+const numRemainder = (array) => {
+  let numFilter = array.filter(Number.isFinite)
+  return numFilter.map(value => value % 3)
+}
+//console.log(numRemainder(testingArray1));
+// Expected output: [ 2, 0, -0, 0 ]
+//console.log(numRemainder(testingArray2));
 // Expected output: [ 2, 1, -1 ]
 
 
@@ -30,3 +87,23 @@ var testingArray2 = [5, "Hola", 43, -34, "greetings", true]
 var testingArray3 = [3, 7, "hi", 10, 3, "hello", 4, "hi"]
 var testingArray4 = [7, "hi", 3, 1, "hi", 4, "hello", 4, 7]
 // Expected output: [ 3, 7, "hi", 10, "hello", 4, 1 ]
+
+
+// Comparing two seperate arrays as arguements (array1, array2)
+// Combine both arrays into one array using .concat 
+// Use * new Set * to store only unique values (so no more repeating values in concat)
+// return result by using the spread operator to expand the iterable object of "set" into a copied array to comply with Expected output
+const noDuplicates = (array1, array2) => {
+  let concat = array1.concat(array2)
+  let set = new Set(concat)
+  return result = [...set]
+}
+//console.log(noDuplicates(testingArray3, testingArray4));
+// Expected output: [ 3, 7, "hi", 10, "hello", 4, 1 ]
+
+
+// const noDuplicates = (array1, array2) => {
+//   return array1 = array1.filter(val => !array2.includes(val))
+// }
+// console.log(noDuplicates(testingArray3, testingArray4));
+// [ 10 ]
--- rails-commenting.rb
+++ rails-commenting.rb
@@ -6,25 +6,25 @@
 
 # FILE: app/controller/blog_posts_controller.rb
 
-# 1)
+# 1) BlogPostsController is a child class inheriting all features of ApplicationController and allows us to add more specific features
 class BlogPostsController < ApplicationController
   def index
-    # 2)
+    # 2) This is where an Active Record call is created that will have all content from the database as an instance variable.
     @posts = BlogPost.all
   end
 
   def show
-    # 3)
+    # 3) This is an Active Record call to "show" a specific blog post by found by id
     @post = BlogPost.find(params[:id])
   end
 
-  # 4)
+  # 4) Defining new, in this case its signalling to developers this is where users can add information to the web application
   def new
     @post = Post.new
   end
 
   def create
-    # 5)
+    # 5) To create an Active Record call to "create" a new user input for Blog Post with blog_post_params
     @post = BlogPost.create(blog_post_params)
     if @post.valid?
       redirect_to blog_post_path(@post)
@@ -39,7 +39,7 @@ class BlogPostsController < ApplicationController
 
   def update
     @post = BlogPost.find(params[:id])
-    # 6)
+    # 6) an "update" Active Record call 
     @post.update(blog_post_params)
     if @post.valid?
       redirect_to blog_post_path(@post)
@@ -53,15 +53,15 @@ class BlogPostsController < ApplicationController
     if @post.destroy
       redirect_to blog_posts_path
     else
-      # 7)
+      # 7)This is if it is not being "destroyed" aka something went wrong... , and reverts back to the specific post 
       redirect_to blog_post_path(@post)
     end
   end
 
-  # 8)
+  # 8) To keep only authorized users from accessing the abilities below the word private. It allows there to be no chance the methods below cannot be called somewhere else in the program
   private
   def blog_post_params
-    # 9)
+    # 9) This is allowing a blog post to be permitted to post if it follows the parameters of a blog post, which are a title and content.
     params.require(:blog_post).permit(:title, :content)
   end
 
@@ -71,6 +71,6 @@ end
 # FILE: app/models/blog_post.rb
 
 class BlogPost < ApplicationRecord
-  # 10)
+  # 10) This allows you to view :comments visibly on the webpage. Creates a has_many relationship between BlogPost and ApplicationRecord so each BlogPost can contain comments 
   has_many :comments
 end
